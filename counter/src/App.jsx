4a747d0301a88deecdb2560543bfe76b71ac097f
Vinay suman <sumanvinay18@gmail.com>
2026-02-24 17:56:30 +0530

    commit all changes

---
+++ counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(15);
+
+  // let counter = 15;
+
+  const addValue = () => {
+    setCounter((prev) => (prev >= 20 ? 20 : prev + 1));
+
+    // counter = counter + 1;
+    // if (counter >= 20) {
+    //   counter = 20;
+    // }
+    // setCounter(counter);
+  };
+
+  const removeValue = () => {
+    setCounter((prev) => (prev <= 0 ? 0 : prev - 1));
+
+    // setCounter(counter - 1);
+    // if (counter <= 0) {
+    //   setCounter(0);
+    // }
+  };
+
+  return (
+    <>
+      <h1>Healthy and code</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value: {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove value: {counter} </button>
+      <footer>Footer: {counter}</footer>
+    </>
+  );
+}
+
+export default App;
